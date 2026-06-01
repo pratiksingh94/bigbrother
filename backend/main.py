4005ac305f2e71f79e51d895dd9131727a7e3be5
pratiksingh94 <pratik.personal4@gmail.com>
2026-06-02 01:04:36 +0530

    setup fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
-from dotenv import load_dotenv
-from utils.db import init_db
 import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+from utils.db import init_db
 
 load_dotenv()
 
 
 init_db(os.environ["DATABASE_URL"])
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "hello fellas :3"}
