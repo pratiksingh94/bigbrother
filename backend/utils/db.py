215bff6920befaa53244201702f9933b401ff6ad
pratiksingh94 <pratik.personal4@gmail.com>
2026-06-02 00:58:16 +0530

    added table creation

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -2,8 +2,24 @@ import psycopg
 
 def init_db(url):
     try:
-        conn = psycopg.connect(url)
+        with psycopg.connect(url) as conn:
+            print("connected to database: ", conn.info.dbname)
+            with conn.cursor() as curr:
+                curr.execute("""
+                CREATE TABLE IF NOT EXISTS agents (
+                    id UUID PRIMARY KEY,
+                    hostname TEXT NOT NULL,
+                    os_name TEXT,
+                    os_version TEXT,
+                    status TEXT NOT NULL,
+                    ip_address TEXT,
+
+                    registered_at TIMESTAMP NOT NULL,
+                    last_seen TIMESTAMP NOT NULL
+                );
+                """)
+                
         
-        print("connected to database")
+        
     except Exception as e:
         print("error while connecting to database:", e)
