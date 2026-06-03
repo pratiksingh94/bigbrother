b2fbb6ba94d77a748a2ad241fa0cb134bd77083d
pratiksingh94 <pratik.personal4@gmail.com>
2026-06-03 21:09:12 +0530

    teeny tiny change

--- lilbro/internal/agent/heartbeat.go
+++ lilbro/internal/agent/heartbeat.go
@@ -10,26 +10,32 @@ import (
 
 func RunHeartbeatLoop(cfg config.Config) {
 
-	go func() {
-		sysinfo := system.GetSysInfo()
-		payload := api.HeartbeatPayload{
-			ID:            cfg.ID,
-			Hostname:      sysinfo.Hostname,
-			KernelVersion: sysinfo.KernelVersion,
-			OSName:        sysinfo.OSName,
+	sysinfo := system.GetSysInfo()
+	payload := api.HeartbeatPayload{
+		ID:            cfg.ID,
+		Hostname:      sysinfo.Hostname,
+		KernelVersion: sysinfo.KernelVersion,
+		OSName:        sysinfo.OSName,
+	}
+
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	// sending once then timer starts
+	err := api.SendHeartbeat(cfg.ServerURL, payload)
+	if err != nil {
+		fmt.Println("error in heartbeat:", err)
+	} else {
+		fmt.Println("heartbeat sent")
+	}
+
+	for range ticker.C {
+		err := api.SendHeartbeat(cfg.ServerURL, payload)
+
+		if err != nil {
+			fmt.Println("error in heartbeat:", err)
+		} else {
+			fmt.Println("heartbeat sent")
 		}
-
-		ticker := time.NewTicker(1 * time.Minute)
-		defer ticker.Stop()
-
-		for range ticker.C {
-			err := api.SendHeartbeat(cfg.ServerURL, payload)
-
-			if err != nil {
-				fmt.Println("error in heartbeat:", err)
-			} else {
-				fmt.Println("heartbeat sent")
-			}
-		}
-	}()
+	}
 }
