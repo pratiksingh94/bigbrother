58661a4832f7ca8fa2db9e171f08ae4457cc5e99
pratiksingh94 <pratik.personal4@gmail.com>
2026-06-06 02:09:31 +0530

    added cors for some testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.models.agent import Agent
 from app.db import Base, engine
@@ -7,6 +8,8 @@ from app.routes.agents import router as agent_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 app.include_router(agent_router, prefix="/agents", tags=["Agents", "lilbro"])
 
